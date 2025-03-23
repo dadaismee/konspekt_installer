274f3b1c9bce7adb81352b403e9b76056c21bf93
Vladimir Yerilov <openmindead@gmail.com>
2025-03-23 19:36:36 +1000

    silence winget and md

--- install.cmd
+++ install.cmd
@@ -1,6 +1,6 @@
 @echo off & setlocal
 chcp 65001
-md C:\test
+md %USERPROFILE%\.konspekt 2>nul
 echo ###Проверка наличия Winget в системе
 where /q winget
 IF ERRORLEVEL 1 (
@@ -11,8 +11,10 @@ IF ERRORLEVEL 1 (
     ECHO Winget найден, отлично...
 )
 
-powershell.exe Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+powershell.exe Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force 2>nul
 
+echo ###Посмотрим, установлен ли Scoop...
+where /q scoop
 IF ERRORLEVEL 1 (
     ECHO Scoop не установлен. Устанавливаю...
 	powershell.exe "Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression"
@@ -44,7 +46,7 @@ IF ERRORLEVEL 1 (
 where /q pandoc
 if errorlevel 1 (
 	echo ###Pandoc не найден, устанавливаю ...
-	winget install -e --id JohnMacFarlane.Pandoc
+	winget install -e --id JohnMacFarlane.Pandoc --silent
 ) else (
 	echo Pandoc уже установлен
 )
@@ -52,16 +54,16 @@ if errorlevel 1 (
 rem set batchPath=%~dp0
 
 echo ###Клонируем репозиторий с настройками Obsidian и Zotero...
-curl.exe -o C:\test\obstest.zip -LC - https://github.com/openmindead/obsidian-test/archive/refs/heads/main.zip
-tar -xf C:\test\obstest.zip -C C:\test
+curl.exe -o %USERPROFILE%\.konspekt\obstest.zip -LC - https://github.com/openmindead/obsidian-test/archive/refs/heads/main.zip
+tar -xf %USERPROFILE%\.konspekt\obstest.zip -C %USERPROFILE%\.konspekt
 
 echo ###Установка Obsidian ...
-if not exist C:\Users\%username%\AppData\Local\Programs\Obsidian\Obsidian.exe winget install -e --id Obsidian.Obsidian
-md %appdata%\obsidian
+if not exist C:\Users\%username%\AppData\Local\Programs\Obsidian\Obsidian.exe winget install -e --id Obsidian.Obsidian --silent
+md %appdata%\obsidian 2>nul
 
 if not exist %appdata%\obsidian\obsidian.json (
     echo Конфиг Obsidian не найден. Используем нашу заготовку...
-	copy C:\test\obsidian-test-main\obsidian_win.json %appdata%\obsidian\obsidian.json
+	copy %USERPROFILE%\.konspekt\obsidian-test-main\obsidian_win.json %appdata%\obsidian\obsidian.json
 	powershell.exe "(Get-Content $env:APPDATA\obsidian\obsidian.json) -replace 'test',$env:USERNAME | Out-File -encoding ASCII $env:APPDATA\obsidian\obsidian.json"
 ) else (
 	>nul find "konspekt_pack" %appdata%\obsidian\obsidian.json && (
@@ -74,8 +76,8 @@ if not exist %appdata%\obsidian\obsidian.json (
 )
 
 if not exist C:\Users\%username%\Documents\konspekt_pack (
-    md C:\Users\%username%\Documents\konspekt_pack
-	xcopy /E C:\test\obsidian-test-main\konspekt_pack C:\Users\%username%\Documents\konspekt_pack
+    md C:\Users\%username%\Documents\konspekt_pack 2>nul
+	xcopy /E %USERPROFILE%\.konspekt\obsidian-test-main\konspekt_pack C:\Users\%username%\Documents\konspekt_pack
     echo Файлы хранилища Obsidian скопированы
 ) else (
     echo Файлы хранилища Obsidian уже на месте
@@ -85,9 +87,9 @@ powershell "obsidian-cli set-default konspekt_pack"
 timeout 1
 
 echo Настраиваю плагины Obsidian...
-copy /Y C:\test\obsidian-test-main\obsidian-pandoc_data_win.json C:\Users\%username%\Documents\konspekt_pack\.obsidian\plugins\obsidian-pandoc\data.json
+copy /Y %USERPROFILE%\.konspekt\obsidian-test-main\obsidian-pandoc_data_win.json C:\Users\%username%\Documents\konspekt_pack\.obsidian\plugins\obsidian-pandoc\data.json
 powershell.exe "(Get-Content C:\Users\$env:USERNAME\Documents\konspekt_pack\.obsidian\plugins\obsidian-pandoc\data.json) -replace 'testuser',$env:USERNAME | Out-File -encoding ASCII C:\Users\$env:USERNAME\Documents\konspekt_pack\.obsidian\plugins\obsidian-pandoc\data.json"
-copy /Y C:\test\obsidian-test-main\obsidian-pandoc-reference-list_data_win.json C:\Users\%username%\Documents\konspekt_pack\.obsidian\plugins\obsidian-pandoc-reference-list\data.json
+copy /Y %USERPROFILE%\.konspekt\obsidian-test-main\obsidian-pandoc-reference-list_data_win.json C:\Users\%username%\Documents\konspekt_pack\.obsidian\plugins\obsidian-pandoc-reference-list\data.json
 powershell.exe "(Get-Content C:\Users\$env:USERNAME\Documents\konspekt_pack\.obsidian\plugins\obsidian-pandoc-reference-list\data.json) -replace 'testuser',$env:USERNAME | Out-File -encoding ASCII C:\Users\$env:USERNAME\Documents\konspekt_pack\.obsidian\plugins\obsidian-pandoc-reference-list\data.json"
 
 echo Сейчас откроется Obsidian, нажмите "Доверять автору" и закройте приложение
@@ -95,14 +97,14 @@ timeout 2
 start /wait C:\Users\%username%\AppData\Local\Programs\Obsidian\Obsidian.exe
 
 echo ###Установка Zotero ...
-if not exist "C:\Program Files\Zotero\zotero.exe" winget install -e --id DigitalScholar.Zotero
+if not exist "C:\Program Files\Zotero\zotero.exe" winget install -e --id DigitalScholar.Zotero --silent
 
 echo ###Загрузка плагинов Зотеро
-curl.exe -o C:\test\bibtex.zip -LC - https://github.com/retorquere/zotero-better-bibtex/releases/download/v7.0.5/zotero-better-bibtex-7.0.5.xpi
-md C:\test\bibtex & tar -xf C:\test\bibtex.zip -C C:\test\bibtex
+curl.exe -o %USERPROFILE%\.konspekt\bibtex.zip -LC - https://github.com/retorquere/zotero-better-bibtex/releases/download/v7.0.5/zotero-better-bibtex-7.0.5.xpi
+md %USERPROFILE%\.konspekt\bibtex 2>nul & tar -xf %USERPROFILE%\.konspekt\bibtex.zip -C %USERPROFILE%\.konspekt\bibtex
 echo ###Загрузка плагина better-bibtex завершена...
-curl.exe -o C:\test\zotmoov.zip -LC - https://github.com/wileyyugioh/zotmoov/releases/download/1.2.11/zotmoov-1.2.11-fx.xpi
-md C:\test\zotmoov & tar -xf C:\test\zotmoov.zip -C C:\test\zotmoov
+curl.exe -o %USERPROFILE%\.konspekt\zotmoov.zip -LC - https://github.com/wileyyugioh/zotmoov/releases/download/1.2.11/zotmoov-1.2.11-fx.xpi
+md %USERPROFILE%\.konspekt\zotmoov 2>nul & tar -xf %USERPROFILE%\.konspekt\zotmoov.zip -C %USERPROFILE%\.konspekt\zotmoov
 echo ###Загрузка плагина zotmoov завершена...
 
 echo Сейчас откроется окно Zotero, закройте его через одну-две секунды
@@ -110,16 +112,16 @@ timeout 2
 powershell "start-process -wait 'C:\Program Files\Zotero\zotero.exe'"
 powershell "Rename-Item $env:USERPROFILE\Zotero\zotero.sqlite $env:USERPROFILE\Zotero\zotero.sqlite.original"
 echo Существующая база Зотеро сохранена как ~/Zotero/zotero.sqlite.original. Вы можете восстановить её, если необходимо, удалив '.original' в названии этого файла.
-copy C:\test\obsidian-test-main\zotero.sqlite %userprofile%\Zotero\zotero.sqlite
+copy %USERPROFILE%\.konspekt\obsidian-test-main\zotero.sqlite %userprofile%\Zotero\zotero.sqlite
 echo Новая библиотека Зотеро установлена вместо существующей
 
 echo Устанавливаю и настраиваю плагины Zotero...
 for /f "tokens=1,2delims=/" %%i in ('powershell "Get-Content $env:APPDATA\Zotero\Zotero\profiles.ini | Select-String -Pattern Path"') do set zotero_profile_name=%%j
-md %appdata%\Zotero\Zotero\Profiles\%zotero_profile_name%\extensions
-md %appdata%\Zotero\Zotero\Profiles\%zotero_profile_name%\extensions\better-bibtex@iris-advies.com
-xcopy /E C:\test\bibtex %appdata%\Zotero\Zotero\Profiles\%zotero_profile_name%\extensions\better-bibtex@iris-advies.com
-md %appdata%\Zotero\Zotero\Profiles\%zotero_profile_name%\extensions\zotmoov@wileyy.com
-xcopy /E C:\test\zotmoov %appdata%\Zotero\Zotero\Profiles\%zotero_profile_name%\extensions\zotmoov@wileyy.com
+md %appdata%\Zotero\Zotero\Profiles\%zotero_profile_name%\extensions 2>nul
+md %appdata%\Zotero\Zotero\Profiles\%zotero_profile_name%\extensions\better-bibtex@iris-advies.com 2>nul
+xcopy /E %USERPROFILE%\.konspekt\bibtex %appdata%\Zotero\Zotero\Profiles\%zotero_profile_name%\extensions\better-bibtex@iris-advies.com
+md %appdata%\Zotero\Zotero\Profiles\%zotero_profile_name%\extensions\zotmoov@wileyy.com 2>nul
+xcopy /E %USERPROFILE%\.konspekt\zotmoov %appdata%\Zotero\Zotero\Profiles\%zotero_profile_name%\extensions\zotmoov@wileyy.com
 type %appdata%\Zotero\Zotero\Profiles\%zotero_profile_name%\prefs.js | findstr /v lastAppVersion | findstr /v lastAppBuildId > prefs.js
 echo user_pref("extensions.zotero.translators.better-bibtex.citekeyFormat", "auth.lower + year");>> prefs.js
 echo user_pref("extensions.zotero.translators.better-bibtex.citekeyFormatEditing", "auth.lower + year");>> prefs.js
@@ -133,7 +135,7 @@ echo Активирую плагины Zotero...
 powershell.exe "(Get-Content $env:%appdata%\Zotero\Zotero\Profiles\%zotero_profile_name%\extensions.json) -replace 'active\":false,\"userDisabled\":true','active\":true,\"userDisabled\":false' | Out-File -encoding ASCII $env:%appdata%\Zotero\Zotero\Profiles\%zotero_profile_name%\extensions.json"
 
 echo 'Настраиваю авто-экспорт библиотеки Zotero...'
-type C:\test\obsidian-test-main\zotero.pref_win.js >> %appdata%\Zotero\Zotero\Profiles\%zotero_profile_name%\prefs.js
+type %USERPROFILE%\.konspekt\obsidian-test-main\zotero.pref_win.js >> %appdata%\Zotero\Zotero\Profiles\%zotero_profile_name%\prefs.js
 powershell.exe "(Get-Content $env:%appdata%\Zotero\Zotero\Profiles\%zotero_profile_name%\prefs.js) -replace 'testuser',$env:USERNAME | Out-File -encoding ASCII $env:%appdata%\Zotero\Zotero\Profiles\%zotero_profile_name%\prefs.js"
 
 echo Вишенка на торте (Citation key)...
@@ -141,7 +143,7 @@ powershell.exe "(Get-Content $env:%appdata%\Zotero\Zotero\Profiles\%zotero_profi
 
 
 echo Очистка временных файлов...
-rd /s /q C:\test
+rd /s /q %USERPROFILE%\.konspekt
 del prefs.js
 echo 'Очистка временных файлов завершена'
 echo 'Установка завершена, всего доброго.'
