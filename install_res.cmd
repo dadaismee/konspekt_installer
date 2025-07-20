de251132df2d96d6a67361b99b601c78db56c428
Vladimir Yerilov <openmindead@gmail.com>
2025-07-20 19:42:17 +1000

    add research variant scripts

---
+++ install_res.cmd
@@ -0,0 +1,215 @@
+@echo off & setlocal
+chcp 65001
+echo Начало работы скрипта...
+pause
+
+powershell "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force"
+md %USERPROFILE%\.konspekt 2>nul
+
+echo ###Проверка наличия Winget в системе
+where /q winget
+IF ERRORLEVEL 1 (
+    ECHO Winget не установлен. Попытка установки...
+	pause
+	powershell.exe "Invoke-WebRequest -Uri https://github.com/microsoft/winget-cli/releases/download/v1.11.400/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile C:\Users\$env:USERNAME\WinGet.msixbundle"
+	ECHO Если что-то пошло не так, вы можете установите его вручную из магазина приложений Windows, или скачать по ссылке "https://github.com/microsoft/winget-cli/releases/download/v1.11.400/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+	set "PATH=%PATH%;%USERPROFILE%\AppData\Local\Microsoft\WindowsApps"
+	powershell.exe "$env:Path += \";$env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\""
+	powershell.exe "[Environment]::SetEnvironmentVariable(\"PATH\", \"$([Environment]::GetEnvironmentVariable(\"PATH\", \"User\"));$env:USERPROFILE\AppData\Local\Microsoft\Windows\", \"User\")"
+    rem EXIT /B
+) ELSE (
+    ECHO Winget найден, отлично...
+)
+
+where /q winget
+IF ERRORLEVEL 1 (
+	ECHO Установка Winget не удалась. Установите его вручную или обновите вашу версию Windows.
+	EXIT /B
+)
+
+echo ###Посмотрим, установлен ли Scoop...
+where /q scoop
+IF ERRORLEVEL 1 (
+    ECHO Scoop не установлен. Устанавливаю...
+	powershell.exe "Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression"
+	set "PATH=%PATH%;%USERPROFILE%\scoop\shims"
+	powershell.exe "[Environment]::SetEnvironmentVariable(\"PATH\", \"$([Environment]::GetEnvironmentVariable(\"PATH\", \"User\"));$env:USERPROFILE\scoop\shims\", \"User\")"
+) ELSE (
+    ECHO Scoop найден, отлично...
+)
+
+echo ###Ищем Git...
+where /q git
+IF ERRORLEVEL 1 (
+    ECHO Git отсутствует. Он тоже нужен, установим его сейчас...
+	powershell.exe "$env:Path += \";$env:USERPROFILE\scoop\shims\"; scoop install git"
+) ELSE (
+    ECHO Git найден, отлично...
+)
+
+echo ###Установка obsidian-cli и tectonic...
+where /q obsidian-cli
+IF ERRORLEVEL 1 (
+    ECHO obsidian-cli не установлен. Устанавливаю...
+	powershell.exe "$env:Path += \";$env:USERPROFILE\scoop\shims\"; scoop bucket add scoop-yakitrak https://github.com/yakitrak/scoop-yakitrak.git; scoop install obsidian-cli"
+) ELSE (
+	ECHO obsidian-cli найден, отлично...
+)
+
+where /q tectonic
+IF ERRORLEVEL 1 (
+    ECHO tectonic не установлен. Устанавливаю...
+	powershell.exe "$env:Path += \";$env:USERPROFILE\scoop\shims\"; scoop install tectonic"
+) ELSE (
+	ECHO tectonic найден, отлично...
+)
+
+where /q pandoc
+if errorlevel 1 (
+	echo ###Pandoc не найден, устанавливаю ...
+	winget install -e --id JohnMacFarlane.Pandoc --silent
+) else (
+	echo Pandoc уже установлен
+)
+
+rem set batchPath=%~dp0
+
+rem echo ###Установка Zettlr ...
+rem if not exist C:\Users\%username%\AppData\Local\Programs\Zettlr\Zettlr.exe (
+rem    winget install -e --id Zettlr.Zettlr --silent
+rem )
+
+echo ###Скачивание репозитория с настройками Obsidian и Zotero...
+curl.exe -o %USERPROFILE%\.konspekt\konspekt.zip -L https://github.com/dadaismee/konspekt-research-pack/archive/refs/heads/main.zip
+tar -xf %USERPROFILE%\.konspekt\konspekt.zip -C %USERPROFILE%\.konspekt
+
+echo ###Установка Obsidian ...
+if not exist C:\Users\%username%\AppData\Local\Programs\Obsidian\Obsidian.exe (
+    winget install -e --id Obsidian.Obsidian --silent
+)
+md %appdata%\obsidian 2>nul
+
+if not exist C:\Users\%username%\research_pack (
+    md C:\Users\%username%\research_pack 2>nul
+	xcopy /eqy %USERPROFILE%\.konspekt\konspekt-research-pack-main\research_pack C:\Users\%username%\research_pack
+    echo Файлы хранилища Obsidian скопированы
+) else (
+    echo Файлы хранилища Obsidian уже на месте, сохраним их под другим именем...
+	powershell "$fdate = Get-Date -format 'yyyyMMdd-hhmmss'; Rename-Item $env:USERPROFILE\research_pack $env:USERPROFILE\research_pack_$fdate; Write-Output 'Файлы хранилища с таким же названием были перемещены'"
+	md C:\Users\%username%\research_pack 2>nul
+	xcopy /eqy %USERPROFILE%\.konspekt\konspekt-research-pack-main\research_pack C:\Users\%username%\research_pack
+    echo Файлы хранилища Obsidian скопированы
+)
+
+echo Сейчас Obsidian будет закрыт.
+pause
+taskkill /f /t /im obsidian.exe
+
+if not exist %appdata%\obsidian\obsidian.json (
+    echo Конфиг Obsidian не найден. Используем нашу заготовку...
+	copy %USERPROFILE%\.konspekt\konspekt-research-pack-main\obsidian_win.json %appdata%\obsidian\obsidian.json
+	powershell.exe "(Get-Content $env:APPDATA\obsidian\obsidian.json) -replace 'test',$env:USERNAME | Out-File -encoding ASCII $env:APPDATA\obsidian\obsidian.json"
+) else (
+	>nul find "research_pack" %appdata%\obsidian\obsidian.json && (
+		echo Хранилище Obsidian с именем research_pack уже существует
+		echo Переименуем его во избежание конфликтов...
+		powershell "$fdate = Get-Date -format 'yyyyMMdd-hhmmss'; Rename-Item $env:APPDATA\obsidian\obsidian.json $env:APPDATA\obsidian\obsidian_$fdate.json; echo 'Старый конфиг хранилища был переименован в '$env:APPDATA\obsidian\obsidian_$fdate.json"
+		copy %USERPROFILE%\.konspekt\konspekt-research-pack-main\obsidian_win.json %appdata%\obsidian\obsidian.json
+		powershell.exe "(Get-Content $env:APPDATA\obsidian\obsidian.json) -replace 'test',$env:USERNAME | Out-File -encoding ASCII $env:APPDATA\obsidian\obsidian.json"
+		echo При необходимости просто снова добавьте ваше старое хранилище вручную
+	) || (
+		echo Добавим наше хранилище Obsidian в существующий конфиг...
+		powershell.exe "(Get-Content $env:APPDATA\obsidian\obsidian.json) -replace ',"open":true}','}'
+		powershell.exe "(Get-Content $env:APPDATA\obsidian\obsidian.json) -replace '}}}','},\"8095a1a7a15b1e3d\":{\"path\":\"C:\\Users\\test\\research_pack\",\"ts\":1739264225722,\"open\":true}},\"showReleaseNotes\":false}' | Out-File -encoding ASCII $env:APPDATA\obsidian\obsidian.json"
+		powershell.exe "(Get-Content $env:APPDATA\obsidian\obsidian.json) -replace '}},','}, \"8095a1a7a15b1e3d\":{\"path\":\"C:\\Users\\test\\research_pack\",\"ts\":1739264225722,\"open\":true}},\"showReleaseNotes\":false}' | Out-File -encoding ASCII $env:APPDATA\obsidian\obsidian.json"
+		powershell.exe "(Get-Content $env:APPDATA\obsidian\obsidian.json) -replace 'test',$env:USERNAME | Out-File -encoding ASCII $env:APPDATA\obsidian\obsidian.json"
+	)
+)
+
+rem %USERPROFILE%\scoop\shims\obsidian-cli set-default research_pack
+powershell "obsidian-cli set-default research_pack"
+timeout 1
+
+echo Настраиваю плагины Obsidian...
+copy /Y %USERPROFILE%\.konspekt\konspekt-research-pack-main\obsidian-pandoc_data_win.json C:\Users\%username%\research_pack\.obsidian\plugins\obsidian-pandoc\data.json
+powershell.exe "(Get-Content C:\Users\$env:USERNAME\research_pack\.obsidian\plugins\obsidian-pandoc\data.json) -replace 'testuser',$env:USERNAME | Out-File -encoding ASCII C:\Users\$env:USERNAME\research_pack\.obsidian\plugins\obsidian-pandoc\data.json"
+copy /Y %USERPROFILE%\.konspekt\konspekt-research-pack-main\obsidian-pandoc-reference-list_data_win.json C:\Users\%username%\research_pack\.obsidian\plugins\obsidian-pandoc-reference-list\data.json
+powershell.exe "(Get-Content C:\Users\$env:USERNAME\research_pack\.obsidian\plugins\obsidian-pandoc-reference-list\data.json) -replace 'testuser',$env:USERNAME | Out-File -encoding ASCII C:\Users\$env:USERNAME\research_pack\.obsidian\plugins\obsidian-pandoc-reference-list\data.json"
+
+echo Сейчас откроется Obsidian, нажмите "Доверять автору" и закройте приложение
+timeout 2
+start /wait C:\Users\%username%\AppData\Local\Programs\Obsidian\Obsidian.exe
+
+echo ###Установка Zotero ...
+if not exist "C:\Program Files\Zotero\zotero.exe" winget install -e --id DigitalScholar.Zotero --silent
+
+echo ###Загрузка плагинов Зотеро
+if not exist %userprofile%\.konspekt\bibtex.zip (
+	curl.exe -o %USERPROFILE%\.konspekt\bibtex.zip -L https://github.com/retorquere/zotero-better-bibtex/releases/download/v7.0.5/zotero-better-bibtex-7.0.5.xpi
+	md %USERPROFILE%\.konspekt\bibtex 2>nul & tar -xf %USERPROFILE%\.konspekt\bibtex.zip -C %USERPROFILE%\.konspekt\bibtex
+	echo ###Загрузка плагина better-bibtex завершена...
+)
+if not exist %userprofile%\.konspekt\zotmoov.zip (
+	curl.exe -o %USERPROFILE%\.konspekt\zotmoov.zip -L https://github.com/wileyyugioh/zotmoov/releases/download/1.2.11/zotmoov-1.2.11-fx.xpi
+	md %USERPROFILE%\.konspekt\zotmoov 2>nul & tar -xf %USERPROFILE%\.konspekt\zotmoov.zip -C %USERPROFILE%\.konspekt\zotmoov
+	echo ###Загрузка плагина zotmoov завершена...
+)
+
+if not exist %userprofile%\Zotero\zotero.sqlite (
+    rem echo Копирую библиотеку Zotero...
+	rem copy %userprofile%\.konspekt\konspekt-research-pack-main\zotero.sqlite %userprofile%\Zotero\zotero.sqlite
+	echo Сейчас откроется окно Zotero, закройте его через одну-две секунды
+	timeout 2
+	powershell "start-process -wait 'C:\Program Files\Zotero\zotero.exe'"
+) else (
+	rem powershell "$fdate = Get-Date -format 'yyyyMMdd-hhmmss'; Rename-Item $env:USERPROFILE\Zotero\zotero.sqlite $env:USERPROFILE\Zotero\zotero_$fdate.sqlite; $oldname1 = '      Существующая база Зотеро сохранена как ~/Zotero/zotero_'; $oldname2 = '.sqlite'; Write-Output $oldname1$fdate$oldname2"
+	rem copy %USERPROFILE%\.konspekt\konspekt-research-pack-main\zotero.sqlite %userprofile%\Zotero\zotero.sqlite
+	rem echo Новая библиотека Зотеро установлена вместо существующей
+)
+
+echo Устанавливаю и настраиваю плагины Zotero...
+for /f "tokens=1,2delims=/" %%i in ('powershell "Get-Content $env:APPDATA\Zotero\Zotero\profiles.ini | Select-String -Pattern Path"') do set zotero_profile_name=%%j
+md %appdata%\Zotero\Zotero\Profiles\%zotero_profile_name%\extensions 2>nul
+if not exist %appdata%\Zotero\Zotero\Profiles\%zotero_profile_name%\extensions\better-bibtex@iris-advies.com (
+	md %appdata%\Zotero\Zotero\Profiles\%zotero_profile_name%\extensions\better-bibtex@iris-advies.com 2>nul
+	xcopy /eqy %USERPROFILE%\.konspekt\bibtex %appdata%\Zotero\Zotero\Profiles\%zotero_profile_name%\extensions\better-bibtex@iris-advies.com
+)
+if not exist %appdata%\Zotero\Zotero\Profiles\%zotero_profile_name%\extensions\zotmoov@wileyy.com (
+	md %appdata%\Zotero\Zotero\Profiles\%zotero_profile_name%\extensions\zotmoov@wileyy.com 2>nul
+	xcopy /eqy %USERPROFILE%\.konspekt\zotmoov %appdata%\Zotero\Zotero\Profiles\%zotero_profile_name%\extensions\zotmoov@wileyy.com
+)
+
+type %appdata%\Zotero\Zotero\Profiles\%zotero_profile_name%\prefs.js | findstr /v lastAppVersion | findstr /v lastAppBuildId > prefs.js
+echo user_pref("extensions.zotero.translators.better-bibtex.citekeyFormat", "auth.lower + year");>> prefs.js
+echo user_pref("extensions.zotero.translators.better-bibtex.citekeyFormatEditing", "auth.lower + year");>> prefs.js
+echo user_pref("extensions.zotmoov.dst_dir", "C:\\Users\\%USERNAME%\\research_pack\\07 service\\literature PDF");>> prefs.js
+copy /y prefs.js %appdata%\Zotero\Zotero\Profiles\%zotero_profile_name%\prefs.js
+
+echo Сейчас для установки плагинов откроется окно Zotero, выйдите из него через одну-две секунды
+timeout 2
+powershell "start-process -wait 'C:\Program Files\Zotero\zotero.exe'"
+echo Активирую плагины Zotero...
+powershell.exe "(Get-Content $env:APPDATA\Zotero\Zotero\Profiles\%zotero_profile_name%\extensions.json) -replace 'active\":false,\"userDisabled\":true','active\":true,\"userDisabled\":false' | Out-File -encoding ASCII $env:APPDATA\Zotero\Zotero\Profiles\%zotero_profile_name%\extensions.json"
+
+echo Настраиваю авто-экспорт библиотеки Zotero...
+type %USERPROFILE%\.konspekt\konspekt-research-pack-main\zotero.pref_win.js >> %appdata%\Zotero\Zotero\Profiles\%zotero_profile_name%\prefs.js
+powershell.exe "(Get-Content $env:APPDATA\Zotero\Zotero\Profiles\%zotero_profile_name%\prefs.js) -replace 'testuser',$env:USERNAME | Out-File -encoding ASCII $env:APPDATA\Zotero\Zotero\Profiles\%zotero_profile_name%\prefs.js"
+
+
+echo Очистка временных файлов...
+rd /s /q %USERPROFILE%\.konspekt
+del prefs.js
+echo Очистка временных файлов завершена
+
+echo Ещё раз откроем Zotero для финальных штрихов, закройте его через секунд 10
+timeout 2
+powershell "start-process -wait 'C:\Program Files\Zotero\zotero.exe'"
+
+echo Попытка настройки отображения Citation key...
+powershell.exe "(Get-Content $env:APPDATA\Zotero\Zotero\Profiles\%zotero_profile_name%\treePrefs.json) -replace 'true}}}','false}}}' | Out-File -encoding ASCII $env:APPDATA\Zotero\Zotero\Profiles\%zotero_profile_name%\treePrefs.json"
+
+echo Установка завершена, открываю Obsidian. Всего доброго!
+timeout 2
+start C:\Users\%username%\AppData\Local\Programs\Obsidian\Obsidian.exe
+
+pause
---
+++ install_res.sh
@@ -0,0 +1,216 @@
+#!/bin/zsh
+
+# bold=$(tput bold)
+# normal=$(tput sgr0)
+
+# Install Homebrew if not installed
+# echo "${bold}Проверяем, установлен ли Homebrew..."
+echo "Проверяем, установлен ли Homebrew..."
+if ! command -v brew &> /dev/null; then
+    echo "Homebrew не найден. Устанавливаем . . ."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+if [ ! -f /Users/$USER/.zprofile ]; then
+    echo >> /Users/$USER/.zprofile
+fi
+arch=$(sysctl -n machdep.cpu.brand_string |cut -w -f1)
+if ! grep 'brew shellenv' /Users/$USER/.zprofile &> /dev/null; then
+    if [[ $arch = "Apple" ]]; then echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$USER/.zprofile; else echo 'eval "$(/usr/local/bin/brew shellenv)"' >> /Users/$USER/.zprofile; fi
+fi
+source ~/.zprofile
+brew_path=$(which brew)
+if [[ $brew_path = "brew not found" ]]; then echo "Не удалось установить Homebrew. Возможно, вам следует отключить или включить ваш VPN, или же подключиться к другой точке доступа / мобильной сети. Попробуем ещё раз."; echo "Нажмите Enter, когда будете готовы . . ."; read -s &>/dev/null; /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"; source ~/.zprofile; brew_path=$(which brew); fi
+
+# Installing packages
+
+which pandoc &> /dev/null
+if [ $? -ne 0 ]; then
+    echo 'Установка Pandoc . . .'
+    brew install pandoc
+else
+    echo "Pandoc уже установлен."
+fi
+pandoc_path=$(which pandoc)
+if [[ $pandoc_path = "pandoc not found" ]]; then echo "Не удалось установить Pandoc. Возможно, вам следует отключить или включить ваш VPN, или же подключиться к другой точке доступа / мобильной сети. Попробуем ещё раз."; echo "Нажмите Enter, когда будете готовы . . ."; read -s &>/dev/null; brew install pandoc; pandoc_path=$(which pandoc); fi
+
+which tectonic &> /dev/null
+if [ $? -ne 0 ]; then
+    echo 'Установка Tectonic . . .'
+    brew install tectonic
+else
+    echo "Tectonic уже установлен."
+fi
+tectonic_path=$(which tectonic)
+if [[ $tectonic_path = "tectonic not found" ]]; then echo "Не удалось установить tectonic. Возможно, вам следует отключить или включить ваш VPN, или же подключиться к другой точке доступа / мобильной сети. Попробуем ещё раз."; echo "Нажмите Enter, когда будете готовы . . ."; read -s &>/dev/null; brew install tectonic; tectonic_path=$(which tectonic); fi
+
+# ls -l /Applications |grep Zettlr &> /dev/null
+# if [ $? -ne 0 ]; then
+#    echo 'Установка Zettlr...'
+#    brew install --cask zettlr
+# else
+#    echo "Zettlr уже установлен."
+# fi
+
+ls -l /Applications |grep Obsidian &> /dev/null
+if [ $? -ne 0 ]; then
+    echo 'Установка Obsidian . . .'
+    brew install --cask obsidian
+else
+    echo "Obsidian уже установлен."
+fi
+obsidian_path=$(ls /Applications |grep Obsidian)
+if [[ $obsidian_path = "" ]]; then echo "Не удалось установить Obsidian. Возможно, вам следует отключить или включить ваш VPN, или же подключиться к другой точке доступа / мобильной сети. Попробуем ещё раз."; echo "Нажмите Enter, когда будете готовы . . ."; read -s &>/dev/null; brew install --cask obsidian; obsidian_path=$(ls /Applications |grep Obsidian); fi
+
+which obsidian-cli &> /dev/null
+if [ $? -ne 0 ]; then
+    echo 'Установка obsidian-cli . . .'
+    brew tap yakitrak/yakitrak
+    brew install yakitrak/yakitrak/obsidian-cli
+else
+    echo "obsidian-cli уже установлен."
+fi
+obcli_path=$(which obsidian-cli)
+if [[ $obcli_path = "obsidian-cli not found" ]]; then echo "Не удалось установить obsidian-cli. Возможно, вам следует отключить или включить ваш VPN, или же подключиться к другой точке доступа / мобильной сети. Попробуем ещё раз."; echo "Нажмите Enter, когда будете готовы . . ."; read -s &>/dev/null; brew tap yakitrak/yakitrak; brew install yakitrak/yakitrak/obsidian-cli; obcli_path=$(which obsidian-cli); fi
+
+echo 'Клонируем репозиторий с настройками Obsidian . . .'
+git clone https://github.com/dadaismee/konspekt-starter-pack.git $HOME/.konspekt
+if [ ! -f $HOME/.konspekt/obsidian.json ]; then	echo "Произошла ошибка при клонировании репозитория с настройками Obsidian. Проверьте подключение к интернету."; echo "Нажмите Enter, когда будете готовы . . ."; read -s &>/dev/null; git clone https://github.com/dadaismee/konspekt-starter-pack.git $HOME/.konspekt; fi
+
+echo "Сейчас Obsidian будет закрыт."
+echo "Нажмите Enter, когда будете готовы . . ."; read -s &>/dev/null
+killall Obsidian
+
+if [ ! -d $HOME/konspekt_pack ]; then
+    cp -R $HOME/.konspekt/konspekt_pack $HOME
+    echo "Файлы хранилища Obsidian скопированы"
+else
+    echo "Файлы хранилища Obsidian уже на месте, сохраним их под другим именем . . ."
+    fdate=$(date +%Y%m%d-%H%M%S)
+    mv $HOME/konspekt_pack $HOME/konspekt_pack_$fdate
+    echo "Файлы хранилища с таким же названием были перемещены"
+    cp -R $HOME/.konspekt/konspekt_pack $HOME
+    echo "Файлы хранилища Obsidian скопированы"
+fi
+
+mkdir -p $HOME/Library/Application\ Support/obsidian
+if [ ! -f $HOME/Library/Application\ Support/obsidian/obsidian.json ]; then
+    echo "Конфиг Obsidian не найден. Используем нашу заготовку . . ."
+    cp $HOME/.konspekt/obsidian.json $HOME/Library/Application\ Support/obsidian/
+    sed -i '' -e "s|test|$(whoami)|g" $HOME/Library/Application\ Support/obsidian/obsidian.json
+else
+    if grep -q konspekt_pack $HOME/Library/Application\ Support/obsidian/obsidian.json; then
+        echo "Хранилище Obsidian с именем konspekt_pack уже существует"
+        echo "Переименуем его во избежание конфликтов"
+        fdate=$(date +%Y%m%d-%H%M%S)
+        mv $HOME/Library/Application\ Support/obsidian/obsidian.json $HOME/Library/Application\ Support/obsidian/obsidian_$fdate.json
+        echo "Старый конфиг хранилищ был переименован в obsidian_$fdate.json"
+        cp $HOME/.konspekt/obsidian.json $HOME/Library/Application\ Support/obsidian/
+        sed -i '' -e "s|test|$(whoami)|g" $HOME/Library/Application\ Support/obsidian/obsidian.json
+		echo "При необходимости просто снова добавьте ваше старое хранилище вручную"
+    else
+        echo "Добавим новое хранилище Obsidian в существующий конфиг . . ."
+		sed -i '' -e "s|,"open":true}|}|g" $HOME/Library/Application\ Support/obsidian/obsidian.json
+        sed -i '' -e "s|}}}|}, \"8095a1a7a15b1e3d\":{\"path\":\"/Users/$USER/konspekt_pack\",\"ts\":1739264225722,\"open\":true}},\"showReleaseNotes\":false}|g" $HOME/Library/Application\ Support/obsidian/obsidian.json
+        sed -i '' -e "s|}},|}, \"8095a1a7a15b1e3d\":{\"path\":\"/Users/$USER/konspekt_pack\",\"ts\":1739264225722,\"open\":true}},\"showReleaseNotes\":false}|g" $HOME/Library/Application\ Support/obsidian/obsidian.json
+    fi
+fi
+
+
+obsidian-cli set-default konspekt_pack
+sleep 1
+
+echo 'Настраиваю плагины Obsidian . . .'
+sed -i '' -e "s|/usr/local/bin/tectonic|$tectonic_path|g" $HOME/konspekt_pack/.obsidian/plugins/obsidian-pandoc/data.json
+sed -i '' -e "s|/usr/local/bin/pandoc|$pandoc_path|g" $HOME/konspekt_pack/.obsidian/plugins/obsidian-pandoc/data.json
+sed -i '' -e "s|/Users/test|/Users/$USER|g" $HOME/konspekt_pack/.obsidian/plugins/obsidian-pandoc/data.json
+sed -i '' -e "s|/usr/local/bin/pandoc|$pandoc_path|g" $HOME/konspekt_pack/.obsidian/plugins/obsidian-pandoc-reference-list/data.json
+sed -i '' -e "s|/Users/test|/Users/$USER|g" $HOME/konspekt_pack/.obsidian/plugins/obsidian-pandoc-reference-list/data.json
+
+open -a Obsidian
+echo 'Сейчас откроется Obsidian, нажмите "Доверять автору" (если будет такой запрос) и закройте приложение через Command+Q'
+sleep 2
+lsof -p $(pgrep -n Obsidian) +r 1 &> /dev/null
+sleep 2
+
+ls -l /Applications |grep Zotero &> /dev/null
+if [ $? -ne 0 ]; then
+	echo 'Установка Zotero...'
+	brew install --cask zotero
+else
+	echo "Zotero уже установлен."
+fi
+zotero_path=$(ls /Applications |grep Zotero)
+if [[ $zotero_path = "" ]]; then echo "Не удалось установить Zotero. Возможно, вам следует отключить или включить ваш VPN, или же подключиться к другой точке доступа / мобильной сети. Попробуем ещё раз."; echo "Нажмите Enter, когда будете готовы . . ."; read -s &>/dev/null; brew install --cask zotero; zotero_path=$(ls /Applications |grep Zotero); fi
+
+echo 'Загрузка плагинов Zotero . . .'
+curl -o $HOME/.konspekt/zotmoov.zip -L https://github.com/wileyyugioh/zotmoov/releases/download/1.2.18/zotmoov-1.2.18-fx.xpi
+if [ ! -f $HOME/.konspekt/zotmoov.zip ]; then echo "Произошла ошибка при загрузке плагина zotmoov. Проверьте подключение к интернету."; echo "Нажмите Enter, когда будете готовы . . ."; read -s &>/dev/null; curl -o $HOME/.konspekt/zotmoov.zip -L https://github.com/wileyyugioh/zotmoov/releases/download/1.2.18/zotmoov-1.2.18-fx.xpi; fi
+echo 'Загрузка плагина zotmoov завершена';
+mkdir -p $HOME/.konspekt/zotmoov
+unzip $HOME/.konspekt/zotmoov.zip -d ~/.konspekt/zotmoov &> /dev/null
+
+curl -o $HOME/.konspekt/bibtex.zip -L https://github.com/retorquere/zotero-better-bibtex/releases/download/v7.0.5/zotero-better-bibtex-7.0.5.xpi
+if [ ! -f $HOME/.konspekt/zotmoov.zip ]; then echo "Произошла ошибка при загрузке плагина bibtex. Проверьте подключение к интернету."; echo "Нажмите Enter, когда будете готовы . . ."; read -s &>/dev/null; $HOME/.konspekt/bibtex.zip -L https://github.com/retorquere/zotero-better-bibtex/releases/download/v7.0.5/zotero-better-bibtex-7.0.5.xpi; fi
+echo 'Загрузка плагина bibtex завершена'
+mkdir -p $HOME/.konspekt/bibtex
+unzip $HOME/.konspekt/bibtex.zip -d ~/.konspekt/bibtex &> /dev/null
+
+if [ ! -f $HOME/Library/Application\ Support/Zotero/profiles.ini ]; then
+    echo 'Сейчас откроется окно Zotero, выйдите из него через Command+Q'
+    open -a Zotero
+    sleep 2
+    lsof -p $(pgrep -n zotero) +r 1 &>/dev/null
+    sleep 2
+fi
+
+if [ ! -f $HOME/Zotero/zotero.sqlite ]; then
+    echo "Копирую библиотеку Zotero..."
+    cp $HOME/.konspekt/zotero.sqlite /Users/$USER/Zotero/zotero.sqlite
+else
+    fdate=$(date +%Y%m%d-%H%M%S)
+    mv /Users/$USER/Zotero/zotero.sqlite /Users/$USER/Zotero/zotero_$fdate.sqlite
+    echo "Существующая база Зотеро сохранена как ~/Zotero/zotero_$fdate.sqlite. Вы можете восстановить её, если необходимо, удалив _$fdate в названии этого файла."
+    cp $HOME/.konspekt/zotero.sqlite /Users/$USER/Zotero/zotero.sqlite
+    echo "Новая библиотека Зотеро установлена вместо существующей"
+fi
+
+echo 'Устанавливаю и настраиваю плагины Zotero...'
+zotero_profile_name=$(grep 'Path=Profiles/' $HOME/Library/Application\ Support/Zotero/profiles.ini | cut -d/ -f2-)
+mkdir -p /Users/$USER/Library/Application\ Support/Zotero/Profiles/$zotero_profile_name/extensions
+cp -R $HOME/.konspekt/zotmoov /Users/$USER/Library/Application\ Support/Zotero/Profiles/$zotero_profile_name/extensions/zotmoov@wileyy.com
+cp -R $HOME/.konspekt/bibtex /Users/$USER/Library/Application\ Support/Zotero/Profiles/$zotero_profile_name/extensions/better-bibtex@iris-advies.com
+#echo /Users/$USER/Documents/Zotero/zotmoov >> /Users/$USER/Library/Application\ Support/Zotero/Profiles/$zotero_profile_name/extensions/zotmoov@wileyy.com
+#echo /Users/$USER/Documents/Zotero/bibtex >> /Users/$USER/Library/Application\ Support/Zotero/Profiles/$zotero_profile_name/extensions/better-bibtex@iris-advies.com
+sed -i '' "/lastAppVersion/d" /Users/$USER/Library/Application\ Support/Zotero/Profiles/$zotero_profile_name/prefs.js
+sed -i '' "/lastAppBuildId/d" /Users/$USER/Library/Application\ Support/Zotero/Profiles/$zotero_profile_name/prefs.js
+echo 'user_pref("extensions.zotero.translators.better-bibtex.citekeyFormat", "auth.lower + year");' >> /Users/$USER/Library/Application\ Support/Zotero/Profiles/$zotero_profile_name/prefs.js
+echo 'user_pref("extensions.zotero.translators.better-bibtex.citekeyFormatEditing", "auth.lower + year");' >> /Users/$USER/Library/Application\ Support/Zotero/Profiles/$zotero_profile_name/prefs.js
+echo 'user_pref("extensions.zotmoov.dst_dir", "/Users/'$USER'/konspekt_pack/07 service/literature PDF");' >> /Users/$USER/Library/Application\ Support/Zotero/Profiles/$zotero_profile_name/prefs.js
+echo 'Окно Zotero откроется для установки плагинов, выйдите из него через Command+Q'
+open -a Zotero
+sleep 2
+lsof -p $(pgrep -n zotero) +r 1 &> /dev/null
+sleep 2
+echo 'Активирую плагины Zotero...'
+sed -i '' -e 's|active":false,"userDisabled":true|active":true,"userDisabled":false|g' $HOME/Library/Application\ Support/Zotero/Profiles/$zotero_profile_name/extensions.json
+sleep 2
+echo 'Настраиваю авто-экспорт библиотеки Zotero...'
+cat $HOME/.konspekt/zotero.pref.js >> $HOME/Library/Application\ Support/Zotero/Profiles/$zotero_profile_name/prefs.js
+sed -i '' -e "s|testuser|$(whoami)|g" $HOME/Library/Application\ Support/Zotero/Profiles/$zotero_profile_name/prefs.js
+
+echo 'Очистка временных файлов...'
+rm -rf $HOME/.konspekt
+echo 'Очистка временных файлов завершена'
+
+echo 'Ещё раз откроем Zotero для финальных штрихов, закройте его через секунд 10'
+open -a Zotero
+sleep 2
+lsof -p $(pgrep -n zotero) +r 1 &> /dev/null
+sleep 2
+
+echo 'Попытка настройки отображения Citation key...'
+#sed -i '' -e "s|better-bibtex-iris-advies-com-citationKey","ordinal":33,"hidden":true|better-bibtex-iris-advies-com-citationKey","ordinal":33,"hidden":false|g" $HOME/Library/Application\ Support/Zotero/Profiles/$zotero_profile_name/prefs.js
+sed -i '' -e "s|true}}}|false}}}|g" $HOME/Library/Application\ Support/Zotero/Profiles/$zotero_profile_name/treePrefs.json
+
+echo 'Установка завершена, открываю Obsidian. Всего доброго!'
+open -a obsidian
